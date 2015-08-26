60cc6ba3ae66b48e0c611da8cb14adbd67ead090
Jerom Dreux <jd@izorian.co.uk>
2015-08-26 12:37:00 +0000

    Updated Styles & Diary

--- public/js/diary.js
+++ public/js/diary.js
@@ -1,188 +1,223 @@
-var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-var weekdays   = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-var today  = new Date();
-var months = [];
-
-function generateCalendar (eventData) {
-  eventData = eventData.sort(function(a, b) { return (new Date(a.startdate)) - (new Date(b.startdate));});
-  generateAllTheMonths(eventData);
-  eventData.forEach(function (event) {
-    appendEvent(event);
-  });
-
-  // Highlight today
-  $('#' + formattedDate(today)).removeClass('no-event').addClass('today');
-  addMonthMenu();
-}
-
-function addMonthMenu() {
-  $('#diary').prepend('<div id="cal-controls">');
-  $('.month-table').each(function(_, table) {
-    var month = $(table).data('month');
-    $('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + month + '</a>');
-  });
-
-  $(document).on('click', '.month-menuitem', function(e) {
-    $('[data-month]').hide();
-    $('[data-month="' + $(this).data('target') + '"]').show();
-    $(this).addClass('active').siblings().removeClass('active');
-    e.preventDefault();
-  });
-
-  // Get current month and click it
-  var currentMonth = $('[data-target=' + (new Date()).getFullYear() + "-"  + monthNames[(new Date()).getMonth()] + ']');
-  if( currentMonth.length ) {
-    currentMonth.click();
-  } else {
-    $('[data-target]').first().click();
-  }
-}
-
-function appendEvent( event ) {
-  var eventStartDate = new Date(event.startdate);
-  var eventEndDate   = new Date(event.enddate);
-  var eventElement   = $('<div class="event"><a href="#">' + event.name + '</a></div>');
-  
-  if (event.details) {
-	  var eventDetails = $('<div/>', {
-		id: "session_" + event.id,
-    	class: 'event-details'
-	  }).appendTo(eventElement);
-	  $('<div/>', {
-		  class: "date",
-    	  text: eventStartDate.toDateString()
-	  }).appendTo(eventDetails);
-	  
-	  var lines = event.details.split("|");
-	  for (var i = 0; i < lines.length; ++i) {
-		  $('<div/>', {
-    		html: lines[i]
-	  	  }).appendTo(eventDetails);
-	  }
-	  if (event.location) $('<div/>', {
-		  class: "location",
-    	  text: event.location
-	  }).appendTo(eventDetails);
-	  eventElement.addClass("session_" + event.id + "_open");
-	  if (event.link) $('<a target="_blank" href="' + event.link + '">Details</a>').appendTo(eventDetails);
-	  //eventElement.attr("onClick", "$('#session" + event.id + "').popup({transition: 'all 0.3s'});");
-  }
+$(function() {
+	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+	var weekdays   = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+	var today  = new Date();
+	var months = [];
+	var root = $(".page")[0];
+	var dataCount = 0;
+	var dataCurrent = 0;
+	var allEventData = null;
 	
-  // Handle multi-days
-  if ( eventEndDate.getDate() ) {
-    var date         = eventStartDate;
-    var spacerNumber = $('#' + formattedDate(eventStartDate)).find('.event').length;
-    eventElement.addClass('multi-days');
+	function cacheThenGenerateCalendar (eventData) {
+		
+		if (!allEventData) {
+			allEventData = eventData;
+		} else {
+			allEventData = allEventData.concat(eventData);
+		}
+		
+		dataCurrent += 1;
+		if (dataCurrent == dataCount){
+			generateCalendar(allEventData);
+		}
+		
+	}
+	
+	function generateCalendar (eventData) {
+	  
+	  eventData = eventData.sort(function(a, b) { return (new Date(a.startdate)) - (new Date(b.startdate));});
+	  generateAllTheMonths(eventData);
+	  eventData.forEach(function (event, index) {
+		appendEvent(event, index);
+	  });
 
-    while ( eventEndDate > date ) {
-      // If reached end of month, go to first day of the next month
-      // Else go to the next day
-      if (date == new Date(date.getFullYear(), date.getMonth() + 1, 0)) {
-        date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
-      } else {
-        date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1 );
-      }
+	  // Highlight today
+	  $('#' + formattedDate(today)).removeClass('no-event').addClass('today');
+	  addMonthMenu();
+	}
 
-      // Add spacer to line up the event
-      var dateElement = $('#' + formattedDate(date));
-      var steps = dateElement.find('.event').length;
-      loopForTimes( spacerNumber - steps, function() {
-        dateElement.append('<div class="event spacer">&nbsp;</div>');
-      });
+	function addMonthMenu() {
+	  $('<div id="cal-controls">').prependTo(root);
+	  $('.month-table').each(function(_, table) {
+		var month = $(table).data('month');
+		var monthDisplay = month.split("-")[1] + ", " + month.split("-")[0];
+		$('#cal-controls').append('<a class="month-menuitem" data-target="' + month + '" href="#' + month + '">' + monthDisplay + '</a>');
+	  });
 
-      dateElement.removeClass('no-event').append('<div class="event multi-days following-days" title="' + event.name + '"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>');
-    }
-  }
+	  $(document).on('click', '.month-menuitem', function(e) {
+		$('[data-month]').hide();
+		$('[data-month="' + $(this).data('target') + '"]').show();
+		$(this).addClass('active').siblings().removeClass('active');
+		e.preventDefault();
+	  });
 
-  $('#' + formattedDate(eventStartDate)).removeClass('no-event').append(eventElement);
-  if (event.details) $("#session_" + event.id).popup();
-}
+	  // Get current month and click it
+	  var currentMonth = $('[data-target=' + (new Date()).getFullYear() + "-"  + monthNames[(new Date()).getMonth()] + ']');
+	  if( currentMonth.length ) {
+		currentMonth.click();
+	  } else {
+		$('[data-target]').first().click();
+	  }
+	}
 
-function generateAllTheMonths( eventData ) {
-  var dates = [];
-  var months = [];
+	function appendEvent(event, index) {
+	  var eventStartDate = new Date(event.startdate);
+	  var eventEndDate   = new Date(event.enddate);
+	  var eventElement   = $('<div class="event"><a href="#">' + event.name + '</a></div>');
 
-  eventData.forEach(function(event) {
-    if (event.startdate) dates.push(event.startdate);
-    if (event.enddate) dates.push(event.enddate);
-  });
+	  if (event.details) {
+		  var eventDetails = $('<div/>', {
+			id: "session_" + index,
+			class: 'event-details'
+		  }).appendTo(eventElement);
+		  $('<div/>', {
+			  class: "date",
+			  text: eventStartDate.toDateString()
+		  }).appendTo(eventDetails);
 
-  dates.forEach(function (date) {
-    date = new Date(date);
-    if(months.indexOf(date.getFullYear().toString() + date.getMonth()) < 0) {
-      months.push(date.getFullYear().toString() + date.getMonth());
-      generateMonthTable(date);
-    } else {
-    }
-  });
-}
+		  var lines = event.details.split("|");
+		  for (var i = 0; i < lines.length; ++i) {
+			  $('<div/>', {
+				html: lines[i]
+			  }).appendTo(eventDetails);
+		  }
+		  if (event.location) $('<div/>', {
+			  class: "location",
+			  text: event.location
+		  }).appendTo(eventDetails);
+		  eventElement.addClass("session_" + index + "_open");
+		  if (event.link) $('<a target="_blank" href="' + event.link + '">Details</a>').appendTo(eventDetails);
+	  }
 
-function generateMonthTable( date ) {
-  var eventMonthName = monthNames[date.getMonth()];
-  var monthTable     = $('<table cellspacing=0 class="month-table" data-month="' + date.getFullYear() + "-"  + eventMonthName + '" id="month-' + date.getMonth() + '"></table>');
-  var monthTableBody = monthTable.append('<tbody>');
-  // var today          = new Date()
-  var endOfToday     = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 00, 00, 00);
-  var firstDay       = new Date(date.getFullYear(), date.getMonth(), 1);
-  var numberOfDays   = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-  var weekDayNumber  = firstDay.getDay();
+	  // Handle multi-days
+	  if ( eventEndDate.getDate() ) {
+		var date         = eventStartDate;
+		var spacerNumber = $('#' + formattedDate(eventStartDate)).find('.event').length;
+		eventElement.addClass('multi-days');
 
-  $('#diary').append(monthTable);
-  monthTable.before('<h2 data-month="' + date.getFullYear() + '-' + eventMonthName + '">' + eventMonthName + ' ' + date.getFullYear() + '</h2>');
+		while ( eventEndDate > date ) {
+		  // If reached end of month, go to first day of the next month
+		  // Else go to the next day
+		  if (date == new Date(date.getFullYear(), date.getMonth() + 1, 0)) {
+			date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+		  } else {
+			date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1 );
+		  }
 
-  // Add month calendar header
-  monthTableBody.append('<tr class="header"></tr>');
-  var headerRow = monthTableBody.find('.header');
-  loopForTimes( 7, function(i) {
-    headerRow.append('<td>' + weekdays[i] + '</td>');
-  });
+		  // Add spacer to line up the event
+		  var dateElement = $('#' + formattedDate(date));
+		  var steps = dateElement.find('.event').length;
+		  loopForTimes( spacerNumber - steps, function() {
+			dateElement.append('<div class="event spacer">&nbsp;</div>');
+		  });
 
-  // Add empty days from previous month
-  var times = weekDayNumber === 0 ? 6 : weekDayNumber - 1;
-  loopForTimes( times, function() {
-    getFirstAvailableRow(monthTable).append('<td class="empty"></td>');
-  });
+		  dateElement.removeClass('no-event').append('<div class="event multi-days following-days" title="' + event.name + '"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>');
+		}
+	  }
 
-  // Filling the month with days
-  loopForTimes( numberOfDays, function(daynumber) {
-    var thisDay = new Date(date.getFullYear(), date.getMonth(), (daynumber + 1));
-    var id = formattedDate(thisDay);
-    var pastClass = endOfToday > thisDay ? " past" : "";
-    getFirstAvailableRow(monthTableBody).append('<td class="no-event' + pastClass + '" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>');
-  });
+	  $('#' + formattedDate(eventStartDate)).removeClass('no-event').append(eventElement);
+	  if (event.details) $("#session_" + index).popup({transition: 'all 0.3s'});
+	}
 
-  // Add empty days from next month
-  var lastRow = monthTable.find('tr:last');
-  var cellsInLastRow = lastRow.find('td').length;
-  // Check if this is necessary
-  if ( cellsInLastRow < 7 ) {
-    loopForTimes( (7 - cellsInLastRow), function() {
-      lastRow.append('<td class="empty"></td>');
-    });
-  }
-}
+	function generateAllTheMonths(eventData) {
+	  var dates = [];
+	  var months = [];
 
-// Because I don't like ot write for()
-function loopForTimes( times, callback ) {
-  for( var i=0; i < times; i++ ){
-    callback(i);
-  }
-}
+	  eventData.forEach(function(event) {
+		if (event.startdate) dates.push(event.startdate);
+		if (event.enddate) dates.push(event.enddate);
+	  });
 
-// This is handy: getting the first row with available cell space
-function getFirstAvailableRow( table ) {
-  var row = table.find('tr.days').filter(function(i, thisRow) {
-    return ($(thisRow).find('td').length) < 7;
-  });
-  // If no available row, create a new one
-  if( row.length === 0 ) {
-    table.append('<tr class=days>');
-    row = table.find('tr').last();
-  }
-  return row;
-}
+	  dates.forEach(function (date) {
+		date = new Date(date);
+		if(months.indexOf(date.getFullYear().toString() + date.getMonth()) < 0) {
+		  months.push(date.getFullYear().toString() + date.getMonth());
+		  generateMonthTable(date);
+		} else {
+		}
+	  });
+	}
 
-// Create an unique date string for cell lookup
-function formattedDate( date ) {
-  return date.getFullYear() + '-' + monthNames[date.getMonth()] + '-' + date.getDate();
-}
+	function generateMonthTable(date) {
+	  var eventMonthName = monthNames[date.getMonth()];
+	  var monthTable     = $('<table cellspacing=0 class="month-table" data-month="' + date.getFullYear() + "-"  + eventMonthName + '" id="month-' + date.getMonth() + '"></table>');
+	  var monthTableBody = monthTable.append('<tbody>');
+	  // var today          = new Date()
+	  var endOfToday     = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+	  var firstDay       = new Date(date.getFullYear(), date.getMonth(), 1);
+	  var numberOfDays   = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+	  var weekDayNumber  = firstDay.getDay();
+
+	  monthTable.appendTo(root);
+	  //monthTable.before('<h2 data-month="' + date.getFullYear() + '-' + eventMonthName + '">' + eventMonthName + ' ' + date.getFullYear() + '</h2>');
+
+	  // Add month calendar header
+	  monthTableBody.append('<tr class="header"></tr>');
+	  var headerRow = monthTableBody.find('.header');
+	  loopForTimes( 7, function(i) {
+		headerRow.append('<td><h2>' + weekdays[i] + '</h2></td>');
+	  });
+
+	  // Add empty days from previous month
+	  var times = weekDayNumber === 0 ? 6 : weekDayNumber - 1;
+	  loopForTimes( times, function() {
+		getFirstAvailableRow(monthTable).append('<td class="empty"></td>');
+	  });
+
+	  // Filling the month with days
+	  loopForTimes( numberOfDays, function(daynumber) {
+		var thisDay = new Date(date.getFullYear(), date.getMonth(), (daynumber + 1));
+		var id = formattedDate(thisDay);
+		var pastClass = endOfToday > thisDay ? " past" : "";
+		getFirstAvailableRow(monthTableBody).append('<td class="no-event' + pastClass + '" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>');
+	  });
+
+	  // Add empty days from next month
+	  var lastRow = monthTable.find('tr:last');
+	  var cellsInLastRow = lastRow.find('td').length;
+	  // Check if this is necessary
+	  if ( cellsInLastRow < 7 ) {
+		loopForTimes( (7 - cellsInLastRow), function() {
+		  lastRow.append('<td class="empty"></td>');
+		});
+	  }
+	}
+
+	// Because I don't like ot write for()
+	function loopForTimes( times, callback ) {
+	  for( var i=0; i < times; i++ ){
+		callback(i);
+	  }
+	}
+
+	// This is handy: getting the first row with available cell space
+	function getFirstAvailableRow( table ) {
+	  var row = table.find('tr.days').filter(function(i, thisRow) {
+		return ($(thisRow).find('td').length) < 7;
+	  });
+	  // If no available row, create a new one
+	  if( row.length === 0 ) {
+		table.append('<tr class=days>');
+		row = table.find('tr').last();
+	  }
+	  return row;
+	}
+
+	// Create an unique date string for cell lookup
+	function formattedDate( date ) {
+	  return date.getFullYear() + '-' + monthNames[date.getMonth()] + '-' + date.getDate();
+	}
+	
+	if (data) {
+		if($.type(data) === "string") {
+			data = [data];
+		}
+		if (data.constructor === Array) {
+			dataCount = data.length;
+			dataCurrent = 0;
+			for(var i = 0; i < data.length; i++ ) {
+				Tabletop.init( { key: data[i], callback: cacheThenGenerateCalendar, simpleSheet: true } );
+	  		}
+		}
+	}
+});
